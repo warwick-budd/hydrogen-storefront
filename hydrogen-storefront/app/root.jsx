c2f9f0f309c5434c709768d9428d03c5fed7fef9
warwick-budd <112562861+warwick-budd@users.noreply.github.com>
2023-06-20 16:12:44 +0100

    demo-store

--- hydrogen-storefront/app/root.jsx
+++ hydrogen-storefront/app/root.jsx
@@ -24,6 +24,101 @@ export const links = () => {
   ];
 };
 
+const CART_QUERY = `#graphql
+  query ($cartId: ID!, $country: CountryCode, $language: LanguageCode)
+    @inContext(country: $country, language: $language) {
+    cart(id: $cartId) {
+      id
+      checkoutUrl
+      totalQuantity
+      lines(first: 100) {
+        edges {
+          node {
+            id
+            quantity
+            cost {
+              totalAmount {
+                amount
+                currencyCode
+              }
+              amountPerQuantity {
+                amount
+                currencyCode
+              }
+              compareAtAmountPerQuantity {
+                amount
+                currencyCode
+              }
+            }
+            merchandise {
+              ... on ProductVariant {
+                id
+                availableForSale
+                compareAtPrice {
+                  ...MoneyFragment
+                }
+                price {
+                  ...MoneyFragment
+                }
+                requiresShipping
+                title
+                image {
+                  ...ImageFragment
+                }
+                product {
+                  handle
+                  title
+                  id
+                }
+                selectedOptions {
+                  name
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+      cost {
+        subtotalAmount {
+          ...MoneyFragment
+        }
+        totalAmount {
+          ...MoneyFragment
+        }
+        totalDutyAmount {
+          ...MoneyFragment
+        }
+        totalTaxAmount {
+          ...MoneyFragment
+        }
+      }
+      note
+      attributes {
+        key
+        value
+      }
+      discountCodes {
+        code
+      }
+    }
+  }
+
+  fragment MoneyFragment on MoneyV2 {
+    currencyCode
+    amount
+  }
+
+  fragment ImageFragment on Image {
+    id
+    url
+    altText
+    width
+    height
+  }
+`;
+
+
 export async function loader({context}) {
   const layout = await context.storefront.query(LAYOUT_QUERY);
   return {layout};
